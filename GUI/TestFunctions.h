a2134a62ac4c52525f542c7be09d7fc2ce68f09e
zouxiaofei <fyshunshun@126.com>
2018-08-09 10:02:02 +0800

    fix the TD icons and driver loading

--- GUI/TestFunctions.h
+++ GUI/TestFunctions.h
@@ -102,3 +102,129 @@ int __cdecl sub_430CD0(void *Src, LPCWSTR lpSubKey, LPCWSTR lpValueName)
 	}
 	return result;
 }
+
+BOOL LoadNTDriver(LPCWSTR lpszDriverName,LPCWSTR lpszDriverPath)
+
+{
+	wchar_t szDriverImagePath[256];
+	//得到完整的驱动路径
+	GetFullPathName(lpszDriverPath, 256, szDriverImagePath, NULL);
+	//s(szDriverImagePath);
+	BOOL bRet = FALSE;
+
+	SC_HANDLE hServiceMgr = NULL;//SCM管理器的句柄
+	SC_HANDLE hServiceDDK = NULL;//NT驱动程序的服务句柄
+
+								 //打开服务控制管理器
+	hServiceMgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+
+	if (hServiceMgr == NULL)
+	{
+		//s(9);
+		bRet = FALSE;
+		goto BeforeLeave;
+	}
+
+	//创建驱动所对应的服务
+	hServiceDDK = CreateService(hServiceMgr,
+		lpszDriverName, //驱动程序的在注册表中的名字  
+		lpszDriverName, // 注册表驱动程序的 DisplayName 值  
+		SERVICE_ALL_ACCESS, // 加载驱动程序的访问权限  
+		SERVICE_KERNEL_DRIVER,// 表示加载的服务是驱动程序  
+		SERVICE_DEMAND_START, // 注册表驱动程序的 Start 值  
+		SERVICE_ERROR_IGNORE, // 注册表驱动程序的 ErrorControl 值  
+		szDriverImagePath, // 注册表驱动程序的 ImagePath 值  
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL);
+
+	DWORD dwRtn;
+
+	//判断服务是否失败
+	if (hServiceDDK == NULL)
+	{
+		dwRtn = GetLastError();
+		if (dwRtn != ERROR_IO_PENDING && dwRtn != ERROR_SERVICE_EXISTS)
+		{
+			//s(99);
+			bRet = FALSE;
+			goto BeforeLeave;
+		}
+		hServiceDDK = OpenService(hServiceMgr, lpszDriverName, SERVICE_ALL_ACCESS);
+		if (hServiceDDK == NULL)
+		{
+			//s(999);
+			bRet = FALSE;
+			goto BeforeLeave;
+		}
+	}
+
+	//开启此项服务
+	bRet = StartService(hServiceDDK, NULL, NULL);
+	if (!bRet)
+	{
+		DWORD dwRtn = GetLastError();
+		if (dwRtn != ERROR_IO_PENDING && dwRtn != ERROR_SERVICE_ALREADY_RUNNING)
+		{
+			//s(9999);
+			bRet = FALSE;
+			goto BeforeLeave;
+		}
+		else
+		{
+			if (dwRtn == ERROR_IO_PENDING)
+			{
+				//s(99999);
+				bRet = FALSE;
+				goto BeforeLeave;
+			}
+			else
+			{
+				bRet = TRUE;
+				goto BeforeLeave;
+			}
+		}
+	}
+	bRet = TRUE;
+	//离开前关闭句柄
+BeforeLeave:
+	if (hServiceDDK)CloseServiceHandle(hServiceDDK);
+	if (hServiceMgr)CloseServiceHandle(hServiceMgr);
+	return bRet;
+}
+
+//卸载驱动程序  
+BOOL UnloadNTDriver(LPCWSTR szSvrName)
+{
+	BOOL bRet = FALSE;
+	SC_HANDLE hServiceMgr = NULL;//SCM管理器的句柄
+	SC_HANDLE hServiceDDK = NULL;//NT驱动程序的服务句柄
+	SERVICE_STATUS SvrSta;
+	//打开SCM管理器
+	hServiceMgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+	if (hServiceMgr == NULL)
+	{
+		bRet = FALSE;
+		goto BeforeLeave;
+	}
+	//打开驱动所对应的服务
+	hServiceDDK = OpenService(hServiceMgr, szSvrName, SERVICE_ALL_ACCESS);
+
+	if (hServiceDDK == NULL)
+	{
+		bRet = FALSE;
+		goto BeforeLeave;
+	}
+	//停止驱动程序，如果停止失败，只有重新启动才能，再动态加载。  
+	ControlService(hServiceDDK, SERVICE_CONTROL_STOP, &SvrSta);
+	//动态卸载驱动程序。  
+	DeleteService(hServiceDDK);
+	bRet = TRUE;
+BeforeLeave:
+	//离开前关闭打开的句柄
+	if (hServiceDDK)CloseServiceHandle(hServiceDDK);
+	if (hServiceMgr)CloseServiceHandle(hServiceMgr);
+	return bRet;
+}
